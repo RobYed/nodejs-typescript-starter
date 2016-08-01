ccdbc1b0fd699d5295b1fec75e2f29559208f05e
Robert Dey <robdey88@gmail.com>
2016-08-01 23:13:05 +0200

    expanded the test example

--- src/models/person.model.ts
+++ src/models/person.model.ts
@@ -1,11 +1,17 @@
 export class Person {
     name: String
+    age: Number
 
-    constructor(name: String) {
+    constructor(name: String, age: Number) {
         this.name = name;
+        this.age = age;
     }
 
     getName(): String {
         return this.name;
-    } 
+    }
+
+    getAge(): Number {
+        return this.age;
+    }
 }
--- test/models/person.model.spec.js
+++ test/models/person.model.spec.js
@@ -3,10 +3,14 @@ describe("Person", function() {
     var person;
 
     beforeEach(function() {
-        person = new Person("Fritz");
+        person = new Person("Fritz", 20);
     });
 
     it("should display its name", function() {
         expect(person.getName()).toBe("Fritz");
     });
+
+    it("should display its age", function() {
+        expect(person.getAge()).toBe(20);
+    })
 })
