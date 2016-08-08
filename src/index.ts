4ceb4f2e11b192e99eb36f3e9b39ca66cc8a01b1
Robert Dey <robdey88@gmail.com>
2016-08-08 22:15:01 +0200

    added public folder for static files

--- src/index.ts
+++ src/index.ts
@@ -11,5 +11,8 @@ let app: express.Application = express();
 // configure http request logging
 app.use( morgan( 'combined', { stream: loggerStream } ) );
 
+// configure serving client as static files
+app.use( express.static('public') );
+
 // start the server
 app.listen(8080, () => { logger.info('Starter server listening on port 8080'); });
