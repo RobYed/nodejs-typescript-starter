23d01732b266e9407b6ba22f043b09d92e22d3be
Robert Dey <robdey88@gmail.com>
2016-08-17 21:14:53 +0200

    added body-parser and improved inline docs

--- src/router.ts
+++ src/router.ts
@@ -1,19 +1,31 @@
 import { Router } from 'express';
+import * as bodyParser from 'body-parser';
 import { booksController } from './controllers/books.controller';
 
 export let router = Router();
 
-// get all books
+// convert http post data to json automatically
+router.use( bodyParser.json() );
+router.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// GET /books
+// Get summary list of all books
 router.get('/books', booksController.getAllBooks);
 
-// create a new book
+// POST /books
+// Create a new book and return it
 router.post('/books', booksController.createBook);
 
-// get book by id
+// GET /books/:bookId
+// Get book by id
 router.get('/books/:bookId', booksController.retrieveBook);
 
-// update an existing book
+// PUT /books/:bookId
+// Update an existing book and return it
 router.put('/books/:bookId', booksController.updateBook);
 
-// delete an existing book
+// DELETE /books/:bookId
+// Delete an existing book
 router.delete('/books/:bookId', booksController.deleteBook);
