1efe42cdcb27b6a2962719e927c104cde34a6cd1
Robert Dey <robdey88@gmail.com>
2016-09-10 19:08:31 +0200

    put book example into own file

---
+++ src/models/book.example.ts
@@ -0,0 +1,16 @@
+export const bookExample = [
+  {
+    "title": "Security for Web Developers: Using JavaScript, HTML, and CSS",
+    "author": ["John Paul Mueller"],
+    "description": "As a web developer, you may not want to spend time making your web app secure, but it definitely comes with the territory.",
+    "datePublished": "2016-01-14T23:00:00.000Z",
+    "ISBN": "978-1491928646"
+  },
+  {
+    "title": "Site Reliability Engineering: How Google Runs Production Systems",
+    "author": ["Betsy Beyer", "Chris Jones", "Jennifer Petoff", "Niall Richard Murphy"],
+    "description": "The overwhelming majority of a software system`s lifespan is spent in use, not in design or implementation.",
+    "datePublished": "2016-04-08T23:00:00.000Z",
+    "ISBN": "978-1491929124"
+  }
+];
--- src/models/book.model.ts
+++ src/models/book.model.ts
@@ -29,25 +29,3 @@ export class Book implements IBook {
     return Book.lastId += 1;
   }
 }
-
-export let BookExamples: Array<IBook> = [
-  new Book(
-    'Security for Web Developers: Using JavaScript, HTML, and CSS',
-    ['John Paul Mueller'],
-    'As a web developer, you may not want to spend time making your web app secure, but it definitely comes with the territory. ' +
-    'This practical guide provides you with the latest information on how to thwart security threats at several levels, ' +
-    'including new areas such as microservices. You will learn how to help protect your app no matter where it runs, ' +
-    'from the latest smartphone to an older desktop, and everything in between.',
-    new Date(2015, 12, 15),
-    '978-1491928646'
-  ),
-  new Book(
-    'Site Reliability Engineering: How Google Runs Production Systems',
-    ['Betsy Beyer', 'Chris Jones', 'Jennifer Petoff', 'Niall Richard Murphy'],
-    'The overwhelming majority of a software system`s lifespan is spent in use, not in design or implementation. ' +
-    ' So, why does conventional wisdom insist that software engineers focus primarily on the design and development of ' +
-    'large-scale computing systems?',
-    new Date(2016, 4, 8),
-    '978-1491929124'
-  )
-];
